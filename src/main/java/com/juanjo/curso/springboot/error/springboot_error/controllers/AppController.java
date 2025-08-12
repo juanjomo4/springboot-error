2793332940c9a532591ef01d8ea7862080f1e492
Juanjo M. <60217633+juanjomo4@users.noreply.github.com>
2025-08-12 08:58:11 +0200

    Control de error 500 NumberFormatException

--- src/main/java/com/juanjo/curso/springboot/error/springboot_error/controllers/AppController.java
+++ src/main/java/com/juanjo/curso/springboot/error/springboot_error/controllers/AppController.java
@@ -8,7 +8,9 @@ public class AppController {
 
     @GetMapping("/app")
     public String index() {
-        int value = 100 / 0;
+        // int value = 100 / 0; Error de división por cero 500
+        // int value = Integer.parseInt("10"); Ok mando un 200
+        int value = Integer.parseInt("10 Hola"); // Error de formato 500 No se puede formatear el String
         System.out.println(value);
         return "Ok mando un 200";
     }
--- src/main/java/com/juanjo/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/juanjo/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -1,10 +1,13 @@
 package com.juanjo.curso.springboot.error.springboot_error.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -28,7 +31,7 @@ public class HandlerExceptionController {
     public ResponseEntity<ErrorModels> notFoundEx(NoHandlerFoundException ex) {
         ErrorModels error = new ErrorModels();
         error.setDate(new Date());
-        error.setError("NoHandlerFoundException, Api Rest no encontrada");
+        error.setError("NoHandlerFoundException, API-Rest no encontrada");
         error.setMessage("Error 404 Not Found. " + ex.getMessage());
         error.setStatus(HttpStatus.NOT_FOUND.value());
 
@@ -36,4 +39,16 @@ public class HandlerExceptionController {
         // return ResponseEntity.status(404).body(error);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
+
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> numberFormatException(Exception ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("Date", new Date().toString());
+        error.put("Error", "NumberFormatException, número no válido");
+        error.put("Message", "Error de formato en el número, no tiene formato de dígito. " + ex.getMessage());
+        error.put("Status", String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()));
+        return error;
+    }
+
 }
